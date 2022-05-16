5352ad06a85b4ea45b87b84ef3dc6530c9f7f42b
Yurii <shnurenko.yuriy@gmail.com>
2022-05-16 14:24:15 +0300

    use XMLHttpRequest

---
+++ practice/calc/js/script.js
@@ -0,0 +1,21 @@
+"use scrict";
+
+const inputUah = document.querySelector('#uah'),
+      inputUsd = document.querySelector('#usd');
+
+inputUah.addEventListener('input', () => {
+    const request = new XMLHttpRequest();
+
+    request.open('GET', 'js/current.json');
+    request.setRequestHeader('Content-type', 'applicatin/json; charset = utf-8');
+    request.send();
+
+    request.addEventListener('load', () => {
+        if (request.status === 200) {
+            const data = JSON.parse(request.response);
+            inputUsd.value = (+inputUah.value / data.current.usd).toFixed(2);
+        } else {
+            inputUsd.value = 'oh)))';
+        }
+    });
+});
