4455fc2ea7766cf17963f4af12a4b4b2a90b3aac
Yurii <shnurenko.yuriy@gmail.com>
2022-05-19 17:15:13 +0300

    use FetchApi in food project

--- practice/food/js/script.js
+++ practice/food/js/script.js
@@ -233,35 +233,38 @@ window.addEventListener('DOMContentLoaded', () => {
         postData(item);
     });
 
-    // Обычный формат
+        //Вариант с FormData
 
     // function postData(form) {
     //     form.addEventListener('submit', (e) => {
     //         e.preventDefault();
 
-    //         const statusMessage = document.createElement('div');
-    //         statusMessage.classList.add('status');
+    //         const statusMessage = document.createElement('img');
+    //         statusMessage.src = message.loading;
     //         statusMessage.textContent = message.loading;
-    //         form.append(statusMessage);
-
-    //         const request = new XMLHttpRequest();
-    //         request.open('POST', 'server.php');
+    //         statusMessage.style.cssText = `
+    //             display: block;
+    //             margin: 0 auto;
+    //         `;
+    //         form.insertAdjacentElement('afterend', statusMessage);
 
     //         const formData = new FormData(form);
-            
-    //         request.send(formData);
 
-    //         request.addEventListener('load', () => {
-    //             if (request.status === 200) {
-    //                 console.log(request.response);
-    //                 statusMessage.textContent = message.success;
-    //                 form.reset();
-    //                 setTimeout(() => {
-    //                     statusMessage.remove();
-    //                 }, 2000);
-    //             } else {
-    //                 statusMessage.textContent = message.failure;
-    //             }
+    //         fetch('server.php', {
+    //             method: "POST",
+    //             body: formData
+    //         })
+    //         .then(data => data.text())
+    //         .then(data => {
+    //             console.log(data);
+    //             showThanksModal(message.success);
+    //             statusMessage.remove();
+    //         })
+    //         .catch(() => {
+    //             showThanksModal(message.failure);
+    //         })
+    //         .finally(() => {
+    //             form.reset();
     //         });
     //     });
     // }
@@ -281,30 +284,31 @@ window.addEventListener('DOMContentLoaded', () => {
             `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-
-            request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
             const object = {};
             formData.forEach(function(value, key){
                 object[key] = value;
             });
-            
-            const json = JSON.stringify(object);
 
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if (request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.success);
-                    form.reset();
-                    statusMessage.remove();
-                } else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.success);
+                statusMessage.remove();
+            })
+            .catch(() => {
+                showThanksModal(message.failure);
+            })
+            .finally(() => {
+                form.reset();
             });
         });
     }
