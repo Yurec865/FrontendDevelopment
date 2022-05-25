f1674ba797a3e5663823242db108c01df96aec08
Yurii <shnurenko.yuriy@gmail.com>
2022-05-25 17:36:58 +0300

    use regexp in food project

--- practice/food/js/script.js
+++ practice/food/js/script.js
@@ -460,11 +460,15 @@ window.addEventListener('DOMContentLoaded', () => {
         dots[slideIndex - 1].style.opacity = 1;
     }
 
+    function deleteNotDigits(str) {
+        return +str.replace(/\D/g, '');
+    }
+
     next.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+        if (offset == deleteNotDigits(width) * (slides.length - 1)) {
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
 
         sildesField.style.transform = `translateX(-${offset}px)`;
@@ -482,9 +486,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
     prev.addEventListener('click', () => {
         if (offset == 0) {
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+            offset = deleteNotDigits(width) * (slides.length - 1);
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
 
         sildesField.style.transform = `translateX(-${offset}px)`;
@@ -505,7 +509,7 @@ window.addEventListener('DOMContentLoaded', () => {
             const slideTo = e.target.getAttribute('data-slide-to');
 
             slideIndex = slideTo;
-            offset = offset = +width.slice(0, width.length - 2) * (slideTo - 1);
+            offset = offset = deleteNotDigits(width) * (slideTo - 1);
 
             sildesField.style.transform = `translateX(-${offset}px)`;
 
