c25cf7d2affc85b5c2bfe46fc96288e58e46209c
Yurii <shnurenko.yuriy@gmail.com>
2022-06-20 16:44:53 +0300

    create new project

--- React/courseapp/src/App.js
+++ React/courseapp/src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+const Header = () => {
+  return <h2>Hello world!</h2>
+}
+
+// const Field = () => {
+//   const holder = 'Enter here';
+//   const styledField = {
+//     width: '300px'
+//   };
+//   return <input placeholder={holder} type="text" style={styledField}/>
+// }
+
+function Btn() {
+  const text = 'log in';
+  const logged = true;
+  
+  return <button>{logged ? 'Enter' : text}</button>
+}
+
+class Field extends React.Component {
+  render() {
+    const holder = 'Enter here';
+    const styledField = {
+    width: '300px'
+    };
+    return <input placeholder={holder} type="text" style={styledField}/>
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Field/>
+      <Btn/>
     </div>
   );
 }
