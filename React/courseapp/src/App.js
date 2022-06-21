b2048bd8be7b855fea85c4f8b9ea2856e9b8c4bc
Yurii <shnurenko.yuriy@gmail.com>
2022-06-21 17:10:49 +0300

    working with class components, state, setState

--- React/courseapp/src/App.js
+++ React/courseapp/src/App.js
@@ -1,19 +1,38 @@
+import {Component} from 'react';
 import './App.css';
 
-function WhoAmI({name, surname, link}) {
-  return (
-    <div>
-      <h1>My name is {name()}, surname - {surname}</h1>
-      <a href={link}>My profile</a>
-    </div>
-  )
+class WhoAmI extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      years: 22,
+      text: '+++'
+    }
+  }
+
+  nextYear = () => {
+    this.setState(state => ({ //передаем колбек если важно предыдущее состояние стейт, если нет то передаем просто обьект
+      years: state.years + 1
+    }))
+  }
+
+  render() {
+    const {name, surname, link} = this.props;
+    return (
+      <div>
+        <button onClick={this.nextYear}>{this.state.text}</button>
+        <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+        <a href={link}>My profile</a>
+      </div>
+    )
+  }
 }
 
 function App() {
   return (
     <div className="App">
-      <WhoAmI name={() => {return 'John'}} surname="Smith" link="facebook.com" />
-      <WhoAmI name={() => {return 'Alex'}} surname="Shepard" link="insta.com" />
+      <WhoAmI name='John' surname="Smith" link="facebook.com" />
+      <WhoAmI name='Alex' surname="Shepard" link="insta.com" />
     </div>
   );
 }
