a6889d057f4c5192316fcacc8fb05731166f5250
Yurii <shnurenko.yuriy@gmail.com>
2022-06-24 18:35:34 +0300

    event in react, this

--- React/courseapp/src/App.js
+++ React/courseapp/src/App.js
@@ -6,23 +6,36 @@ class WhoAmI extends Component {
     super(props);
     this.state = {
       years: 22,
-      text: '+++'
+      text: '+++',
+      position: ''
     }
   }
 
   nextYear = () => {
     this.setState(state => ({ //передаем колбек если важно предыдущее состояние стейт, если нет то передаем просто обьект
-      years: state.years + 1
+      years: state.years + 1 //стейт возвращает обьект но тут мы работает только с йеарс без текст и не стоит волноваться стейт изменяет только то с чем мы работаем 
     }))
+  }
+
+  commitInputChanges = (e, color) => {
+    console.log(color);
+    this.setState({
+      position: e.target.value
+    })
   }
 
   render() {
     const {name, surname, link} = this.props;
+    const {position, years} = this.state;
     return (
       <div>
         <button onClick={this.nextYear}>{this.state.text}</button>
-        <h1>My name is {name}, surname - {surname}, age - {this.state.years}</h1>
+        <h1>My name is {name}, surname - {surname}, age - {years}, position - {position}</h1>
         <a href={link}>My profile</a>
+        <form>
+          <span>Введите должность</span>
+          <input type="text" onChange={(e) => this.commitInputChanges(e, 'some color')} />
+        </form>
       </div>
     )
   }
