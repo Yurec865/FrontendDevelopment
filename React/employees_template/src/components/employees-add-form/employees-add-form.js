eb45c8b10decad62b23144725f2cedd1c6e0df10
Yurii <shnurenko.yuriy@gmail.com>
2022-06-25 18:03:18 +0300

    immutability and own events

--- React/employees_template/src/components/employees-add-form/employees-add-form.js
+++ React/employees_template/src/components/employees-add-form/employees-add-form.js
@@ -10,6 +10,15 @@ class EmployeesAddForm extends Component {
         }
     }
 
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdd(this.state.name, this.state.salary);
+        this.setState({
+            name: '',
+            salary: ''
+        })
+    }
+
     onValueChange = (e) => {
         this.setState({
             [e.target.name]: e.target.value
@@ -23,7 +32,8 @@ class EmployeesAddForm extends Component {
             <div className="app-add-form">
                 <h3>Добавьте нового сотрудника</h3>
                 <form
-                    className="add-form d-flex">
+                    className="add-form d-flex"
+                    onSubmit={this.onSubmit}>
                     <input type="text"
                         className="form-control new-post-label"
                         placeholder="Как его зовут?"
