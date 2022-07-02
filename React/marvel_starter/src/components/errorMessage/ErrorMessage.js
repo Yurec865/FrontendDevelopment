d9764ccbf0793cc61393ccdd6b9f7fe80628add5
Yurii <shnurenko.yuriy@gmail.com>
2022-07-02 19:40:19 +0300

    component lifecycle

--- React/marvel_starter/src/components/errorMessage/ErrorMessage.js
+++ React/marvel_starter/src/components/errorMessage/ErrorMessage.js
@@ -1,10 +1,10 @@
 import img from './error.gif';
 
-const ErrorMassage = () => {
+const ErrorMessage = () => {
     return (
         <img style={{ display: 'block', width: "250px", height: "250px", objectFit:
          'contain', margin: "0 auto"}} src={img} alt="Error"/>
     )
 }
 
-export default ErrorMassage;
+export default ErrorMessage;
