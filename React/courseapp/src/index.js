40675b6eb31d66b031dabb047334b0f5a2140695
Yurii <shnurenko.yuriy@gmail.com>
2022-07-06 18:13:03 +0300

    use ref and tabIndex

--- React/courseapp/src/index.js
+++ React/courseapp/src/index.js
@@ -6,20 +6,20 @@ import BootstrapTest from './BootstrapTest';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import styled from 'styled-components';
-import {Button} from './App';
+//import styled from 'styled-components';
+//import {Button} from './App';
 
-const BigButton = styled(Button)`
-  margin: 0 auto;
-  width: 245px;
-  text-align: center;
-`;
+// const BigButton = styled(Button)`
+//   margin: 0 auto;
+//   width: 245px;
+//   text-align: center;
+// `;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
     <App/>
-    <BigButton as="a">+++</BigButton>
+    {/* <BigButton as="a">+++</BigButton> */}
     <BootstrapTest/>
   </StrictMode>
 );
