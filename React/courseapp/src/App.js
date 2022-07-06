c0a7aad8607202b1bfba845cf3d899abbf7c2c51
Yurii <shnurenko.yuriy@gmail.com>
2022-07-06 16:35:59 +0300

    composition

--- React/courseapp/src/App.js
+++ React/courseapp/src/App.js
@@ -91,9 +91,20 @@ const DynamicGreating = (props) => {
     )
 }
 
+const HelloGreating = () => {
+    return (
+        <div style={{'width': '600px', 'margin': '0 auto'}}>
+            <DynamicGreating color={'primary'}>
+                <h2>This weel was hard</h2>
+            </DynamicGreating>
+        </div>    
+    )
+}
+
 function App() {
   return (
     <Wrapper>
+        <HelloGreating/>
         <BootstrapTest
             left = {
                 <DynamicGreating color={'primary'}>
