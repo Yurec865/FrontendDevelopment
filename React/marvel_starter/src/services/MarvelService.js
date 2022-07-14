0312943449bd50c8210eeb6d800da68a3acd8059
Yurii <shnurenko.yuriy@gmail.com>
2022-07-14 19:30:44 +0300

    dinamic import and React.lazy

--- React/marvel_starter/src/services/MarvelService.js
+++ React/marvel_starter/src/services/MarvelService.js
@@ -31,7 +31,7 @@ const useMarvelService = () => {
         return {
             id: char.id,
             name: char.name,
-            description: char.description ? `${char.description.slice(0, 228)}...` : 'There is no description for this character',
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
             thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
             homepage: char.urls[0].url,
             wiki: char.urls[1].url,
