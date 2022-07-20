c82368327a2f5d8bfc622736f6682b97cc07a2ed
Yurii <shnurenko.yuriy@gmail.com>
2022-07-20 15:40:03 +0300

    Finite-state machine

--- React/marvel_starter/src/services/MarvelService.js
+++ React/marvel_starter/src/services/MarvelService.js
@@ -1,7 +1,7 @@
 import {useHttp} from '../hooks/http.hook';
 
 const useMarvelService = () => {
-    const {loading, request, error, clearError} = useHttp();
+    const {request, clearError, process, setProcess} = useHttp();
 
     const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
     const _apiKey = 'apikey=2a7f518a5ab65a6322c0a351effc2d1e';
@@ -56,7 +56,14 @@ const useMarvelService = () => {
         }
     }
 
-    return {loading, error, getAllCharacters, getCharacter, getCharacterByName, clearError, getAllComics, getComic};
+    return {process, 
+            setProcess,
+            getAllCharacters, 
+            getCharacter, 
+            getCharacterByName, 
+            clearError, 
+            getAllComics, 
+            getComic}
 }
 
 export default useMarvelService;
