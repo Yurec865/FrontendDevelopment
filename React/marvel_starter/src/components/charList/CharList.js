1f5c3e10f4cad6f6ec4fdcad9af2a92ef11733b8
Yurii <shnurenko.yuriy@gmail.com>
2022-07-20 16:39:53 +0300

    fixing bugs in third-party libraries

--- React/marvel_starter/src/components/charList/CharList.js
+++ React/marvel_starter/src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import {useState, useEffect, useRef} from 'react';
+import {useState, useEffect, useRef, useMemo} from 'react';
 import PropTypes from 'prop-types';
 import {CSSTransition, TransitionGroup} from 'react-transition-group';
 
@@ -34,6 +34,7 @@ const CharList = (props) => {
 
     useEffect(() => {
         onRequest(offset, true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     const onRequest = (offset, initial) => {
@@ -101,9 +102,14 @@ const CharList = (props) => {
         )
     }
 
+    const elements = useMemo(() => {
+        return setContent(process, () => renderItems(charList), newItemLoading);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [process])
+
     return (
         <div className="char__list">
-            {setContent(process, () => renderItems(charList), newItemLoading)}
+            {elements}
             <button 
                 disabled={newItemLoading} 
                 style={{'display' : charEnded ? 'none' : 'block'}}
--- React/marvel_starter/src/components/comicsList/ComicsList.js
+++ React/marvel_starter/src/components/comicsList/ComicsList.js
@@ -29,10 +29,11 @@ const ComicsList = () => {
     const [offset, setOffset] = useState(0);
     const [comicsEnded, setComicsEnded] = useState(false);
 
-    const {loading, error, getAllComics, process, setProcess} = useMarvelService();
+    const {getAllComics, process, setProcess} = useMarvelService();
 
     useEffect(() => {
         onRequest(offset, true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     const onRequest = (offset, initial) => {
