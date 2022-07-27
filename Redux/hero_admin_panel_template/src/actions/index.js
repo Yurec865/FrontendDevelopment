4216852e804d9d9e2b1b8f6bab91815c41070ea5
Yurii <shnurenko.yuriy@gmail.com>
2022-07-27 18:14:19 +0300

    redux toolkit: createReducer()

--- Redux/hero_admin_panel_template/src/actions/index.js
+++ Redux/hero_admin_panel_template/src/actions/index.js
@@ -14,28 +14,11 @@ export const fetchFifters = (request) => (dispatch) => {
             .catch(() => dispatch(filtersFetchingError()))
 }
 
-// export const heroesFetching = () => {
-//     return {
-//         type: 'HEROES_FETCHING'
-//     }
-// }
-
 export const heroesFetching = createAction('HEROES_FETCHING');
-
-// export const heroesFetched = (heroes) => {
-//     return {
-//         type: 'HEROES_FETCHED',
-//         payload: heroes
-//     }
-// }
-
 export const heroesFetched = createAction('HEROES_FETCHED');
-
-export const heroesFetchingError = () => {
-    return {
-        type: 'HEROES_FETCHING_ERROR'
-    }
-}
+export const heroesFetchingError = createAction('HEROES_FETCHING_ERROR')
+export const heroCreated = createAction('HERO_CREATED');
+export const heroDeleted = createAction('HERO_DELETED');
 
 export const filtersFetching = () => {
     return {
@@ -60,19 +43,5 @@ export const activeFilterChanged = (filter) => {
     return {
         type: 'ACTIVE_FILTER_CHANGED',
         payload: filter
-    }
-}
-
-export const heroCreated = (hero) => {
-    return {
-        type: 'HERO_CREATED',
-        payload: hero
-    }
-}
-
-export const heroDeleted = (id) => {
-    return {
-        type: 'HERO_DELETED',
-        payload: id
     }
 }
